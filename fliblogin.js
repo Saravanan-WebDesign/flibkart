b2e2ed062b8f04372c8c39e44b1c8e9e43549789
Saravanan-WebDesign <saravanan111pc@gmail.com>
2024-05-30 19:41:26 +0530

    This is FlibKart Demo with javascript login

---
+++ fliblogin.js
@@ -0,0 +1,15 @@
+var myText = document.getElementById("mytextvalue")
+var mypass = document.getElementById("myPassword").value
+var myconfrom = document.getElementById("conformPass").value
+var phone = document.getElementById("myNumber")
+
+
+
+
+
+
+
+ 
+
+
+
